8ef8f6424bbe7993bf74603d9f97f165a578efb8
PARAG MODI <77090196+justforhost26@users.noreply.github.com>
2021-06-30 17:05:49 +0530

    Update views.py

--- task/views.py
+++ task/views.py
@@ -13,5 +13,10 @@ def index(request):
     images=Images.objects.filter(website=id)
     sliders=Sliders.objects.filter(website=id)
     menus=Menu.objects.filter(website=id)
-    footer=Footer.objects.get(website=id)
+    try:
+        footer=Footer.objects.get(website=id)
+    except:
+        footer=""
+
+        
     return render(request,"index.html",{"datas":data[0].name_of_website,"images":images,"sliders":sliders,"menus":menus,"footer":footer})
